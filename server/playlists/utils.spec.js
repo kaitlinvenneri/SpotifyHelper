9e6a2c22fdbc1f1be7f6719209dd9913f1b4b853
Kaitlin <kaitlinvenneri@outlook.com>
2022-02-27 22:31:08 -0500

    Check for coverage now when running tests. Also added more tests

--- server/playlists/utils.spec.js
+++ server/playlists/utils.spec.js
@@ -9,7 +9,41 @@ describe('Put Liked Tracks in Map', () => {
     const result = utils.putLikedTracksInMap(input);
     const artistsFromResult = result.get(song.name.toUpperCase());
 
+    expect(result.size).toBe(input.length);
     expect(artistsFromResult).toContain(song.artists[0]);
-    //expect(artistsFromResult).toBe(song.artists[0]);
+  });
+
+  test('should put songs with the same name into the map as a single song with multiple artists', () => {
+    const song1 = { name: 'song 1', artists: ['artist1'] };
+    const song2 = { name: 'song 1', artists: ['artist2'] };
+
+    const input = [song1, song2];
+
+    const result = utils.putLikedTracksInMap(input);
+    const artistsFromResult = result.get(song1.name.toUpperCase());
+
+    expect(result.size).toBe(1);
+    expect(artistsFromResult).toContain(song1.artists[0]);
+  });
+
+  test('should put songs with different names into the map as a multiple songs', () => {
+    const song1 = { name: 'song 1', artists: ['artist1'] };
+    const song2 = { name: 'song 2', artists: ['artist2'] };
+
+    const input = [song1, song2];
+
+    const result = utils.putLikedTracksInMap(input);
+    const artistsFromResult = result.get(song1.name.toUpperCase());
+
+    expect(result.size).toBe(input.length);
+    expect(artistsFromResult).toContain(song1.artists[0]);
+  });
+
+  test('should return an empty map when given an empty array', () => {
+    const input = [];
+
+    const result = utils.putLikedTracksInMap(input);
+
+    expect(result.size).toBe(input.length);
   });
 });
